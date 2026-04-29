c26bacf4b6d38299c7fd686157f0da2879c365a7
Lukas Jääger <81866926+LuComic@users.noreply.github.com>
2026-04-29 22:11:30 +0300

    got the ui down

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<div class="flex flex-col gap-4">
+	<h1 class="title text-xl lg:text-3xl">Write something and send it to the void</h1>
+	<textarea class="h-auto w-full rounded-md bg-zinc-900 lg:text-lg"></textarea>
+	<button
+		class="mr-auto w-auto rounded-md bg-white/90 px-2 py-0.5 font-medium text-black hover:bg-white/70"
+		>Send</button
+	>
+</div>
+<div class="mt-10 w-full rounded-md border border-zinc-900 px-2 py-1 text-zinc-300 lg:text-lg">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum iusto quibusdam nostrum quo,
+	debitis temporibus sed nesciunt ab animi est nobis! Quo delectus cum corrupti repellendus illum
+	quaerat fugiat voluptatem labore, fugit dolores obcaecati assumenda sint illo magnam, quis
+	voluptas, eligendi velit dolorem voluptatum laudantium molestias iste rem ipsum saepe.
+</div>
+
+<style>
+	textarea {
+		field-sizing: content;
+		min-height: 100px;
+		padding: 2px 4px;
+		outline-color: rgba(255, 255, 255, 50%);
+		outline-style: dotted;
+	}
+
+	.title {
+		font-family: 'Merriweather Variable', serif;
+	}
+</style>
